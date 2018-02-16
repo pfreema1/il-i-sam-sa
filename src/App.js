784fb43a8a01992af412f2443f0de52e799e8320
Paul <pfreema1@gmail.com>
2018-02-16 16:14:54 -0500

    redux added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import { applyMiddleware, createStore } from 'redux';
+import logger from 'redux-logger';
 import Sequencer from './Sequencer';
 import './App.css';
 
+const initialState = {
+  foo: 1
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer, applyMiddleware(logger));
+
 class App extends Component {
   render() {
-    return <Sequencer />;
+    return (
+      <Provider store={store}>
+        <Sequencer />
+      </Provider>
+    );
   }
 }
 
