a7ed0de3b06f63c40ec0ea0158d7ee5adebf8504
Paul <pfreema1@gmail.com>
2018-03-20 16:13:29 -0400

    play-control styling done

--- src/Components/Toolbar/PlayControlView.js
+++ src/Components/Toolbar/PlayControlView.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import playIcon from '../../icons/playIcon.svg';
 
 const PlayControlView = ({
   handlePlayButtonClick,
@@ -11,24 +12,36 @@ const PlayControlView = ({
     <div className="play-control__song-pattern-select">
       <div
         onClick={handlePlayBackModeClick.bind(null, 'song')}
-        className={
-          'play-control__song-select ' +
-          (playBackMode === 'song' ? 'play-control__play-mode-selected' : '')
-        }
+        className={'play-control__song-select '}
       >
-        <div className="play-control__small-box-select">[]</div>
-        <div className="play-control__song-pattern-text">Song</div>
+        <div className="play-control__small-box-select">
+          <div
+            className={
+              'play-control__indicator ' +
+              (playBackMode === 'song'
+                ? 'play-control__play-mode-selected'
+                : '')
+            }
+          />
+        </div>
+        <div className="play-control__song-pattern-text">SONG</div>
       </div>
 
       <div
         onClick={handlePlayBackModeClick.bind(null, 'pattern')}
-        className={
-          'play-control__patttern-select ' +
-          (playBackMode === 'pattern' ? 'play-control__play-mode-selected' : '')
-        }
+        className={'play-control__pattern-select '}
       >
-        <div className="play-control__small-box-select">[]</div>
-        <div className="play-control__song-pattern-text">Pattern</div>
+        <div className="play-control__small-box-select">
+          <div
+            className={
+              'play-control__indicator ' +
+              (playBackMode === 'pattern'
+                ? 'play-control__play-mode-selected'
+                : '')
+            }
+          />
+        </div>
+        <div className="play-control__song-pattern-text">PATTERN</div>
       </div>
     </div>
 
@@ -39,11 +52,19 @@ const PlayControlView = ({
         (isPlaying ? 'play-control__is-playing' : '')
       }
     >
-      {isPlaying ? '||' : '>'}
+      {isPlaying ? (
+        '||'
+      ) : (
+        <img
+          className="play-control__play-icon"
+          src={playIcon}
+          alt="play icon"
+        />
+      )}
     </div>
 
     <div onClick={handleStopButtonClick} className="play-control__stop-button">
-      []
+      <div className="play-control__stop-button-icon" />
     </div>
   </div>
 );
