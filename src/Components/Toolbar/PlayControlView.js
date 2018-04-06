143bec9a75855edb1ecd6e698d753a99ef230dc7
pfreema1 <pfreema1@gmail.com>
2018-04-05 20:35:51 -0400

    added touchstart and onmousedown handling for playcontrolview

--- src/Components/Toolbar/PlayControlView.js
+++ src/Components/Toolbar/PlayControlView.js
@@ -1,27 +1,36 @@
-import React from 'react';
-import playIcon from '../../icons/playIcon.svg';
-import pauseIcon from '../../icons/pauseIcon.svg';
+import React from "react";
+import playIcon from "../../icons/playIcon.svg";
+import pauseIcon from "../../icons/pauseIcon.svg";
+import PropTypes from "prop-types";
 
-const PlayControlView = ({
-  handlePlayButtonClick,
-  handleStopButtonClick,
-  handlePlayBackModeClick,
-  isPlaying,
-  playBackMode
-}) => (
+const PlayControlView = (
+  {
+    handlePlayButtonClick,
+    handleStopButtonClick,
+    handlePlayBackModeClick,
+    isPlaying,
+    playBackMode
+  },
+  context
+) => (
   <div className="play-control-wrapper">
     <div className="play-control__song-pattern-select">
       <div
-        onClick={handlePlayBackModeClick.bind(null, 'song')}
-        className={'play-control__song-select '}
+        onMouseDown={
+          context.mobileViewportContext
+            ? () => {}
+            : handlePlayBackModeClick.bind(null, "song")
+        }
+        onTouchStart={handlePlayBackModeClick.bind(null, "song")}
+        className={"play-control__song-select "}
       >
         <div className="play-control__small-box-select">
           <div
             className={
-              'play-control__indicator ' +
-              (playBackMode === 'song'
-                ? 'play-control__play-mode-selected'
-                : '')
+              "play-control__indicator " +
+              (playBackMode === "song"
+                ? "play-control__play-mode-selected"
+                : "")
             }
           />
         </div>
@@ -29,16 +38,21 @@ const PlayControlView = ({
       </div>
 
       <div
-        onClick={handlePlayBackModeClick.bind(null, 'pattern')}
-        className={'play-control__pattern-select '}
+        onMouseDown={
+          context.mobileViewportContext
+            ? () => {}
+            : handlePlayBackModeClick.bind(null, "pattern")
+        }
+        onTouchStart={handlePlayBackModeClick.bind(null, "pattern")}
+        className={"play-control__pattern-select "}
       >
         <div className="play-control__small-box-select">
           <div
             className={
-              'play-control__indicator ' +
-              (playBackMode === 'pattern'
-                ? 'play-control__play-mode-selected'
-                : '')
+              "play-control__indicator " +
+              (playBackMode === "pattern"
+                ? "play-control__play-mode-selected"
+                : "")
             }
           />
         </div>
@@ -47,15 +61,18 @@ const PlayControlView = ({
     </div>
 
     <div
-      onClick={handlePlayButtonClick}
-      className={'play-control__play-pause-button '}
+      onMouseDown={
+        context.mobileViewportContext ? () => {} : handlePlayButtonClick
+      }
+      onTouchStart={handlePlayButtonClick}
+      className={"play-control__play-pause-button "}
     >
       {isPlaying ? (
         <img
           className="play-control__play-icon"
           src={pauseIcon}
           alt="pause icon"
-          style={{ marginLeft: '-2px' }}
+          style={{ marginLeft: "-2px" }}
         />
       ) : (
         <img
@@ -66,10 +83,20 @@ const PlayControlView = ({
       )}
     </div>
 
-    <div onClick={handleStopButtonClick} className="play-control__stop-button">
+    <div
+      onMouseDown={
+        context.mobileViewportContext ? () => {} : handleStopButtonClick
+      }
+      onTouchStart={handleStopButtonClick}
+      className="play-control__stop-button"
+    >
       <div className="play-control__stop-button-icon" />
     </div>
   </div>
 );
+
+PlayControlView.contextTypes = {
+  mobileViewportContext: PropTypes.bool
+};
 
 export default PlayControlView;
